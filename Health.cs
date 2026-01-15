85ea583bcc71382bf34acda1561dd541e5a4b72e
Kazooie56 <w0465155@campus.nscc.ca>
2026-01-15 00:20:43 -0400

    Initial commit

---
+++ Health.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProgII_Challenge1HealthSystemRevisited_NickPD
+{
+    internal class Health
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public int CurrentHealth { get; private set; }
+        public int MaxHealth { get; private set; }
+
+        public void TakeDamage(int damage)
+        {
+            if (damage < 0)
+            {
+                Console.WriteLine("Damage cannot be negative.");
+                return;
+            }
+            if (damage > CurrentHealth)
+            {
+                damage = CurrentHealth;
+            }
+
+            CurrentHealth -= damage;
+        }
+
+        public void Restore()
+        {
+            CurrentHealth = MaxHealth;
+        }
+
+        public void Heal(int healing)
+        {
+            if (healing < 0)
+            {
+                Console.WriteLine("Healing cannot be negative.");
+                return;
+            }
+            if (CurrentHealth + healing > MaxHealth)
+            {
+                CurrentHealth = MaxHealth;
+            }
+
+            CurrentHealth += healing;
+        }
+
+        public Health(int maxHealth)
+        {
+            MaxHealth = maxHealth;
+            CurrentHealth = maxHealth;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProgII_Challenge1HealthSystemRevisited_NickPD
+{
+    internal class Player
+    {
+        // don't need to write public get; public set; because the string is public.
+        public string Name { get; set; }
+        public Health Health { get; private set; }
+        public Health Shield { get; private set; }
+
+        public void TakeDamage(int damage)
+        {
+            Shield -= damage;
+            // _shield?
+        }
+    }
+}
